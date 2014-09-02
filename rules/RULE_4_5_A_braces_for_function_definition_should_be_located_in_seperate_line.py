930ae857ba05d2d913e61b251e59a4f4dde433c3
kunaltyagi <tyagi.kunal@live.com>
2014-09-03 03:00:28 +0530

    Little changes for brances inside functions in proper places

--- rules/RULE_4_5_A_braces_for_function_definition_should_be_located_in_seperate_line.py
+++ rules/RULE_4_5_A_braces_for_function_definition_should_be_located_in_seperate_line.py
@@ -4,20 +4,20 @@ Braces for function definition should be located in the seperate line.
 == Violation ==
 
     void A() { <== Violation
-    
+
     }
-    
+
     void A()
     {
       } <== Violation. It has different columns.
 
 == Good ==
 
-    void A() 
+    void A()
     { <== OK
     }
-    
-    void K() 
+
+    void K()
     {
        while(True) { <== Don't care
        }
@@ -29,19 +29,19 @@ from nsiqcppstyle_reporter import *
 from nsiqcppstyle_rulemanager import *
 
 
-def RunRule(lexer, fullName, decl, contextStack, typeContext) :  
+def RunRule(lexer, fullName, decl, contextStack, typeContext) :
     if not decl and typeContext != None :
         t = lexer.GetNextTokenInType("LBRACE", False, True)
         if t != None :
             t2 = typeContext.endToken
-            if t2 != None and t.lineno != t2.lineno : 
+            if t2 != None and t.lineno != t2.lineno :
                 prevToken = lexer.GetPrevTokenSkipWhiteSpaceAndCommentAndPreprocess()
                 #print contextStack.Peek()
                 if prevToken != None and prevToken.lineno == t.lineno :
                     nsiqcppstyle_reporter.Error(t, __name__, "The brace for function definition should be located in start of line")
                 if t2.lineno != t.lineno and GetRealColumn(t2) != GetRealColumn(t) :
                     nsiqcppstyle_reporter.Error(t2, __name__, "The brace for function definition should be located in same column")
-                    
+
 ruleManager.AddFunctionNameRule(RunRule)
 
 ###########################################################################################
@@ -52,7 +52,7 @@ from nsiqunittest.nsiqcppstyle_unittestbase import *
 
 class testRule(nct):
     def setUpRule(self):
-        ruleManager.AddFunctionNameRule(RunRule)   
+        ruleManager.AddFunctionNameRule(RunRule)
     def test1(self):
         self.Analyze("thisfile.c","""
 void function() {
@@ -63,29 +63,29 @@ void function() {
     def test2(self):
         self.Analyze("thisfile.c","""
 void function() const {
-    
+
 }
 """)
         assert CheckErrorContent(__name__)
     def test3(self):
         self.Analyze("thisfile.c","""
 class K {
-    void function() const 
+    void function() const
     {
-        
+
     }
 }
 """)
         assert not CheckErrorContent(__name__)
     def test4(self):
         self.Analyze("thisfile.c","""
-void function() 
+void function()
 {
     while(True) {
     }
 }
 class A {
-void function() 
+void function()
   {
   }
 }
@@ -94,7 +94,7 @@ void function()
     def test5(self):
         self.Analyze("thisfile.c","""
 class K {
-    void function() const 
+    void function() const
     {   }
 }
 """)
