d46f3c903e834f1322f41787e9569fc65116d30a
Kunal Tyagi <tyagi.kunal@live.com>
2017-09-21 22:28:54 +0900

    Added tests for PR #5

--- rules/RULE_3_2_H_do_not_use_underbars_for_cpp_filename.py
+++ rules/RULE_3_2_H_do_not_use_underbars_for_cpp_filename.py
@@ -6,11 +6,16 @@ Only alphabets, numbers can be used for a cpp filename.
 == Vilolation ==
 
     /testdir/test_1.cpp <== Violation. - is used.
-    /testdir1/_test1.cpp <== Violation. _ is used 
+    /testdir1/_test1.cxx <== Violation. _ is used
+    /testdir/test_1.cc <== Violation. - is used.
+    /testdir1/_test1.mm <== Violation. _ is used
 
 == Good ==
 
-    testdir/test.cpp 
+    testdir/test.cpp
+    testdir/test.cxx
+    testdir/test.cc
+    testdir/test.mm
     testdir1/test_1.c <== Don't care. it's c file.
 """
 
@@ -19,8 +24,8 @@ from nsiqcppstyle_reporter import *
 from nsiqcppstyle_rulemanager import *
 
 def RunRule(lexer, filename, dirname) :
-    if Search("[_]", filename) and filename[filename.rfind("."):] in (".cpp", ".cxx", "cc", "mm") :
-        nsiqcppstyle_reporter.Error(DummyToken(lexer.filename, "", 0,0), __name__, 
+    if Search("[_]", filename) and filename[filename.rfind("."):] in (".cpp", ".cxx", ".cc", ".mm") :
+        nsiqcppstyle_reporter.Error(DummyToken(lexer.filename, "", 0,0), __name__,
                            "Do not use underbar for cpp file name (%s)." % filename)
 
 ruleManager.AddFileStartRule(RunRule)
@@ -40,15 +45,33 @@ from nsiqunittest.nsiqcppstyle_unittestbase import *
 class testRule(nct):
     def setUpRule(self):
         ruleManager.AddFileStartRule(RunRule)
-    
+
     def test1(self):
-        self.Analyze("test/this_file.cpp", "")
-        assert CheckErrorContent(__name__)    
-    def test2(self):
         self.Analyze("test/thisfile.cpp", "")
         assert not CheckErrorContent(__name__)
-
-    def test3(self):
+    def test2(self):
         self.Analyze("test/this_file.c", "")
         assert not CheckErrorContent(__name__)
-    
+    def test3(self):
+        self.Analyze("test/thisfile.cxx", "")
+        assert not CheckErrorContent(__name__)
+    def test4(self):
+        self.Analyze("test/thisfile.cc", "")
+        assert not CheckErrorContent(__name__)
+    def test5(self):
+        self.Analyze("test/thisfile.mm", "")
+        assert not CheckErrorContent(__name__)
+
+    def test6(self):
+        self.Analyze("test/this_file.cxx", "")
+        assert CheckErrorContent(__name__)
+    def test7(self):
+        self.Analyze("test/this_file.cpp", "")
+        assert CheckErrorContent(__name__)
+    def test8(self):
+        self.Analyze("test/this_file.cc", "")
+        assert CheckErrorContent(__name__)
+    def test9(self):
+        self.Analyze("test/this_file.mm", "")
+        assert CheckErrorContent(__name__)
+
