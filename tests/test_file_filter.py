ff8d02ab8bd6c1c4cf818f4f1a9e9e9db4c51c08
Kunal Tyagi <2657068+kunaltyagi@users.noreply.github.com>
2024-10-27 14:18:16 +0900

    Add more tests for file pattern (#74)

--- tests/test_file_filter.py
+++ tests/test_file_filter.py
@@ -15,6 +15,11 @@ def test_non_recursive_exclude(simple_filter: Filter):
     simple_filter.AddExclude("*.py")
     assert not simple_filter.CheckFileInclusion("hello.py")
     assert simple_filter.CheckFileInclusion("SomeDir/hello.py")
+    assert simple_filter.CheckFileInclusion("ParentDir/SomeDir/hello.py")
+
+    simple_filter.AddExclude("*/*.py")
+    assert not simple_filter.CheckFileInclusion("SomeDir/hello.py")
+    assert simple_filter.CheckFileInclusion("ParentDir/SomeDir/hello.py")
 
 
 @pytest.mark.skipif(sys.version_info < (3, 13), reason="Needs new glob features from 3.13")
@@ -22,6 +27,15 @@ def test_recursive_exclude(simple_filter: Filter):
     simple_filter.AddExclude("**/*.py")
     assert not simple_filter.CheckFileInclusion("hello.py")
     assert not simple_filter.CheckFileInclusion("SomeDir/hello.py")
+    assert not simple_filter.CheckFileInclusion("ParentDir/SomeDir/hello.py")
+
+
+@pytest.mark.skipif(sys.version_info < (3, 13), reason="Needs new glob features from 3.13")
+def test_mix_exclude(simple_filter: Filter):
+    simple_filter.AddExclude("test/**/*.py")
+    assert simple_filter.CheckFileInclusion("hello.py")
+    assert not simple_filter.CheckFileInclusion("test/hello.py")
+    assert not simple_filter.CheckFileInclusion("test/SomeDir/hello.py")
 
 
 def test_prefix_match(simple_filter: Filter):
